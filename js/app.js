1d5f1d66be0b2489478f94ba756d042712bbcfd5
Saif Uddin Ahmed <neononezero@gmail.com>
2021-09-13 02:32:52 +0600

    Initial environment setup is successfully done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products`;
+  // const url = `https://fakestoreapi.com/products`;
+  const url = `js/api.json`;
+
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
