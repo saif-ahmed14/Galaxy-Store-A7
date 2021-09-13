9ba92657c45f9b9aa2633b0ed458613adb21f47d
Saif Uddin Ahmed <neononezero@gmail.com>
2021-09-14 02:42:41 +0600

    Latest update successfully added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 const loadProducts = () => {
-  // const url = `https://fakestoreapi.com/products`;
-  const url = `js/api.json`;
-
+  const url = `https://fakestoreapi.com/products`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
@@ -10,36 +8,48 @@ loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
-  const allProducts = products.map((pd) => pd);
+  document.getElementById('all-products').innerHTML = '';
+  const allProducts = products.map(product => product);
+
   for (const product of allProducts) {
-    const image = product.images;
-    const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    console.log(product);
+    const image = product.image;
+    const div = document.createElement('div');
+    div.classList.add('col', 'product');
+    div.innerHTML = `
+      <div class="single-product"> <div>
+      <img class="product-image" src=${image}></img>
+      
       <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
+      <h5 class="title-height my-4">${product.title}</h5>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+      <h2 class="mb-2">Price: $ ${product.price}</h2>
+      <p>Rating - ${product.rating.rate}
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      (${product.rating.count})</p>
+      <button onclick="addToCart(${product.id}, ${product.price})" id="addToCart-btn" class="buy-now btn text-white btn-bg-color">Add to cart</button>
+      <button id="details-btn" class="btn btn-secondary text-white">Details</button>
+      </div>`;
     document.getElementById("all-products").appendChild(div);
   }
 };
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
+  updateTotal ();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -48,12 +58,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
